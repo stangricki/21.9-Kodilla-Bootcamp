5221e98eb6bdee3c54ec4bd27f66e3c56a969382
Filip Stangricki <stangricki@gmail.com>
2018-02-15 19:22:31 +0100

    Commit

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var mongoose = require('mongoose');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-/*mongoose.connect('mongodb://stangricki:password-2@ds237748.mlab.com:37748/database-2', {
+mongoose.connect('mongodb://stangricki:password-2@ds237748.mlab.com:37748/database-2', {
     useMongoClient: true
-});*/
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Conected to DataBase.");
+    }
+});
 
 /*
 testcollection
