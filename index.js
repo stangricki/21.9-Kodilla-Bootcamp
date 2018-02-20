c0d670f76c3586a7fc644568dfe54de192d879c4
Filip Stangricki <stangricki@gmail.com>
2018-02-20 12:16:01 +0100

    Added logic for create, update and delete data methods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
@@ -20,6 +21,8 @@ express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
   // on start
   .get("/", (req, res) => getData(req, res))
   // on LOAD button
@@ -27,32 +30,36 @@ express()
     getData(req, res);
     res.redirect('/');
   })
+
   // on INSERT button
   .post("/insert", (req, res, next) => {
-    // TUTAJ!!!!!!!
     var item = {
       name: req.body.name,
       surname: req.body.surname
     };
-    User.insert(item)
+    var data = new User(item);
+    data.save()
     res.redirect('/');
   })
   // on UPDATE button
   .post("/update", (req, res, next) => {
-    var item = {
-      name: req.body.name,
-      surname: req.body.surname,
-      id: ''//?????
+    var id = req.body.id;
+
+    User.findById(id, function(err, doc) {
+    if (err) {
+      console.error('error, no entry found');
     }
-    User.update(item)
-    res.redirect('/');
+    doc.name = req.body.name;
+    doc.surname = req.body.surname;
+    doc.save();
   })
+  res.redirect('/');
+  })
+
   // on DELETE button
   .post("/delete", (req, res, next) => {
-    var item = {
-      id: ''//?????
-    };
-    User.delete(item)
+   var id = req.body.id;
+    User.findByIdAndRemove(id).exec();
     res.redirect('/');
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
